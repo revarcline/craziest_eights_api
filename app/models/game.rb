0b6dd7f06e2edb2978685d850bb9d2a5bee0840b
Alex Cline <rev.a.r.cline@gmail.com>
2021-07-28 12:41:50 -0500

    player cards working

--- app/models/game.rb
+++ app/models/game.rb
@@ -5,15 +5,15 @@ class Game < ApplicationRecord
   attr_reader :total_players
 
   def add_player(name, is_ai)
-    player = Player.create(name: name, game: self, is_ai: is_ai)
-    Pile.create(deck: deck, player: player, role: 'hand')
+    Player.create(name: name, game: self, is_ai: is_ai)
   end
 
   def start_game
     number = 2 if players.length > 4
+    number |= 1
     Deck.create(game: self, number: number)
-    update(state: 'active')
     deal
+    update(state: 'active')
   end
 
   def stock
@@ -29,7 +29,12 @@ class Game < ApplicationRecord
     stock.top_card.move(discard)
   end
 
+  private
+
   def deal
+    players.each do |player|
+      Pile.create(deck: deck, player: player, role: 'hand')
+    end
     5.times { players.each(&:draw_from_stock) }
   end
 end
