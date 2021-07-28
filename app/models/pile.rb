aa474e554a936f792dca06f2d0531796a74ec74a
Alex Cline <rev.a.r.cline@gmail.com>
2021-07-28 11:02:43 -0500

    cards can shuffle

--- app/models/pile.rb
+++ app/models/pile.rb
@@ -1,10 +1,17 @@
 class Pile < ApplicationRecord
   belongs_to :deck
-  belongs_to :player
+  belongs_to :player, optional: true
   has_many :cards
 
   def shuffle
-    card_ids = cards(&:id)
+    cards.pluck(:id).sort_by { rand }.each { |id| Card.find(id).touch }
+  end
 
+  def ordered_cards
+    cards.order(updated_at: :desc)
+  end
+
+  def move(card, new_pile)
+    card.update(pile: new_pile)
   end
 end
