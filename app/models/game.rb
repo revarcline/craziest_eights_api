5c925046a7d1988b2b1fe37d72039fd09d12840e
Alex Cline <rev.a.r.cline@gmail.com>
2021-07-28 12:51:46 -0500

    attr_readers instead of db queries

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,26 +2,16 @@ class Game < ApplicationRecord
   has_many :players, dependent: :destroy
   has_one :deck, dependent: :destroy
   has_many :piles, through: :deck
-  attr_reader :total_players
+  attr_reader :stock, :discard
 
   def add_player(name, is_ai)
     Player.create(name: name, game: self, is_ai: is_ai)
   end
 
   def start_game
-    number = 2 if players.length > 4
-    number |= 1
-    Deck.create(game: self, number: number)
+    make_deck
     deal
-    update(state: 'active')
-  end
-
-  def stock
-    piles.find_by(role: 'stock')
-  end
-
-  def discard
-    piles.find_by(role: 'discard')
+    update(state: 'active', turn: 0)
   end
 
   def discard_to_stock
@@ -36,5 +26,13 @@ class Game < ApplicationRecord
       Pile.create(deck: deck, player: player, role: 'hand')
     end
     5.times { players.each(&:draw_from_stock) }
+  end
+
+  def make_deck
+    number = 2 if players.length > 4
+    number |= 1
+    Deck.create(game: self, number: number)
+    @stock = piles.find_by(role: 'stock')
+    @discard = piles.find_by(role: 'discard')
   end
 end
