3c19d9874082c9e7b825ff936c2c723f669f8f9a
Alex Cline <rev.a.r.cline@gmail.com>
2021-07-28 14:21:57 -0500

    player count in api

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,4 +3,9 @@ class GamesController < ApplicationController
     games = Game.where(state: 'pending')
     render json: games.to_json(methods: :player_count)
   end
+
+  def player_count
+    game = Game.find(params[:id])
+    render json: game.to_json(only: [], methods: :player_count)
+  end
 end
