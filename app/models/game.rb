7f24284e1350c4d1b52e08f0617427dfe0649872
Alex Cline <rev.a.r.cline@gmail.com>
2021-08-01 14:43:47 -0500

    scaffolding jbuilder views

--- app/models/game.rb
+++ app/models/game.rb
@@ -6,7 +6,8 @@ class Game < ApplicationRecord
   accepts_nested_attributes_for :players
 
   def add_player(name, is_ai)
-    Player.create(name: name, game: self, is_ai: is_ai)
+    Player.new(name: name, game: self, is_ai: is_ai)
+    player if player.save
   end
 
   def current_player
