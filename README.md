4db701be8e688cf03446f2112d16d0c822962aa0
Alex Cline <rev.a.r.cline@gmail.com>
2021-08-01 17:46:38 -0500

    games index showing

--- README.md
+++ README.md
@@ -1,15 +1,28 @@
 # Crazy Eights API
 
-## Routes
-* `get '/games'`: Index all pending games
-* `get '/games/:game_id/player/:player_id'`, to: 'games#show'
-post '/games', to: 'games#create'
-post '/games/:game_id/new_player', to: 'games#new_player'
-post '/games/:game_id/start/player/:player_id', to: 'games#start'
-post '/games/:game_id/finish/player/:player_id', to: 'games#finish'
-delete '/games/:game_id/player/:player_id', to: 'games#destroy'
-get '/players/:player_id', to: 'players#show'
+## Example Deploy
+An example deployment is available 
+
+## Routes, Requests, and Responses
+There are two primary controllers for `craziest-eights`, `games` and `players`.
+
+All authenticated routes require an `auth_token` parameter at the root level. This token is automatically generated when a new player is created. It is only returned once, in the responses to `post /games` and `post /games/:game_id/new_player`, and must be saved by the user or client. For SPAs and other clients, a good option is to keep it in the browser's `localStorage` along with `player_id` and `game_id`.
+
+All games that have not been interacted with for over 6 hours are wiped from the server at midnight (0:00) UTC.
+
+####Games
+* `get '/games'`: Index all pending games.
+* `get '/games/:game_id/player/:player_id'`: Show game details. Authenticated route.
+* `post '/games'`: Create new game and player.
+* `post '/games/:game_id/new_player'`: Create new player for game if game state is `'pending'`.
+* `post '/games/:game_id/start/player/:player_id'`: Set game to `'active'` and deal cards. Authenticated route.
+* `post '/games/:game_id/finish/player/:player_id'`: Set game to `'complete'`, whether or not a winner exists. Authenticated route.
+* `delete '/games/:game_id/player/:player_id'`: Destroy game if state is not `'active'`. Authenticated route.
+
+####Players
+* `get '/players/:player_id'`: 
 post '/players/:id/draw', to: 'players#draw'
 post '/players/:id/play', to: 'players#play'
 
-All authenticated routes require an `auth_token` parameter at the root level. This token is automatically generated whenever a new player is created.
+## Sample API library
+An example of an API libarary using JavaScript `fetch` calls is available at `/sample/userApi.js`. It is currently pointing to the example deployment at `https://craziest-eights.buckar.ooo` but the `API_ROOT` constant can be changed for local usage.
--- example/userApi.js
+++ example/userApi.js
@@ -1,39 +1,29 @@
-export const ROOT_URL = "http://localhost:3000";
+const ROOT_URL = "http://localhost:3000";
 
-const jsonHeaders = (authTrue) => {
-  let headers = {
-    headers: {
-      Accept: "application/json",
-      "Content-type": "application/json; charset=UTF-8",
-    },
-  };
-  // if true append auth header
-  if (authTrue) {
-    headers.headers.Authorization = `Bearer ${localStorage.getItem(
-      "authToken",
-    )}`;
-  }
-  return headers;
+const jsonHeaders = {
+  headers: {
+    Accept: "application/json",
+    "Content-type": "application/json; charset=UTF-8",
+  },
 };
 
-const postOpts = (authTrue) => {
-  return { ...jsonHeaders(authTrue), method: "POST" };
-};
+const postOpts = { ...jsonHeaders, method: "POST" };
 
 const userApi = {
   game: {
     index: () =>
       fetch(`${ROOT_URL}/games`, {
-        ...jsonHeaders(false),
+        ...jsonHeaders,
       }).then((response) => response.json()),
 
     show: (game, player) =>
       fetch(`${ROOT_URL}/games/${game}/player/${player}`, {
-        ...jsonHeaders(true),
+        ...jsonHeaders,
       }).then((response) => response.json()),
+
     create: (name, playerName) =>
       fetch(`${ROOT_URL}/games`, {
-        ...postOpts(false),
+        ...postOpts,
         body: JSON.stringify({
           game: {
             name: name,
@@ -44,25 +34,29 @@ const userApi = {
           },
         }),
       }).then((response) => response.json()),
+
     // player obj: player: {name: string, is_ai: boolean}
     newPlayer: (game, player) =>
       fetch(`${ROOT_URL}/games/${game}/new_player`, {
-        ...postOpts(false),
+        ...postOpts,
         body: JSON.stringify({ player: player }),
       }).then((response) => response.json()),
+
     start: (game, player) =>
       fetch(`${ROOT_URL}/games/${game}/start/player/${player}`, {
-        ...postOpts(true),
+        ...postOpts,
         body: JSON.stringify({}),
       }).then((response) => response.json()),
+
     finish: (game, player) =>
       fetch(`${ROOT_URL}/games/${game}/finish/player/${player}`, {
-        ...postOpts(true),
+        ...postOpts,
         body: JSON.stringify({}),
       }).then((response) => response.json()),
+
     destroy: (game, player) =>
       fetch(`${ROOT_URL}/games/${game}/player/${player}`, {
-        ...jsonHeaders(true),
+        ...jsonHeaders,
         method: "DELETE",
       }).then((response) => response.json()),
   },
@@ -70,19 +64,23 @@ const userApi = {
   player: {
     show: (player) =>
       fetch(`${ROOT_URL}/players/${player}`, {
-        ...jsonHeaders(true),
+        ...jsonHeaders,
       }).then((response) => response.json()),
+
     drawCard: (player) =>
       fetch(`${ROOT_URL}/players/${player}/draw`, {
-        ...postOpts(true),
+        ...postOpts,
         body: JSON.stringify({}),
       }).then((response) => response.json()),
+
     playCard: (player, card) =>
       fetch(`${ROOT_URL}/players/${player}/play`, {
-        ...postOpts(true),
+        ...postOpts,
         body: JSON.stringify({ card: card }),
       }).then((response) => response.json()),
   },
 };
+
+// examples of keeping client details in localStorage on player join
 
 export default userApi;
